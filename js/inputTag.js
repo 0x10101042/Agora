eebe6d062c0c28a8789173524fa61ba70f88c872
Beatryx <baba.beubeu@gmail.com>
2020-12-07 18:33:40 +0100

    AddFileter
    -add tags profile
    -blacklisted tags
    feature tags

--- js/inputTag.js
+++ js/inputTag.js
@@ -1,9 +1,23 @@
 function addTagOnList(){
     var tags = document.getElementById("inputTag").value;
-    if(tags.includes(',')){
-        var list = document.getElementById("listTags");
-        var tagToList = tags.replace(',','');
-        list.innerHTML += '<button type="button" class="btn btn-success" style="margin: 10px" id="like_tag" onclick="deleteLikeTag()">'+tagToList+'</button>';
-        document.getElementById("inputTag").value='';
+    var list = document.getElementById("listTags");
+    if(tags.includes(',') || tags.includes(';')){
+        var tagToList = tags.replace(',','').replace(';','');
+        if(tagToList!=''){
+            list.innerHTML += '<button type="button" class="btn btn-success" style="margin: 10px" id="like_tag" onclick="deleteLikeTag()">'+tagToList+'</button>';
+            document.getElementById("inputTag").value='';
+        }
+    }
+}function dislikeTagFromPeople(){
+    var tags = document.getElementById("inputTag").value;
+    var list = document.getElementById("listTags");
+    if(tags.includes(',') || tags.includes(';')){
+        var tagToList = tags.replace(',','').replace(';','');
+        if(tagToList!=''){
+            list.innerHTML += '<button type="button" class="btn btn-danger" style="margin: 10px" id="like_tag" onclick="deleteLikeTag()">'+tagToList+'</button>';
+            document.getElementById("inputTag").value='';
+        }
     }
 }
+
+
